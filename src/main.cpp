1159c44523943158937fc04fe193d44f83cf8a9a
Stephen Berry <stephenberry.developer@gmail.com>
2024-05-15 15:59:25 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,8 @@ using namespace ut;
 suite tests = [] {
    "my_test"_test = []() mutable {
       double x = 42;
-      ut::expect(42.1 == x) << "x is not 42.1";
-      ut::expect[42.1 == x];
+      expect(42.1 == x) << "x is not 42.1";
+      expect[42.1 == x];
    };
 };
 
