7ec5ff251b1151f1de568c796060456ef7e031ea
Stephen Berry <stephenberry.developer@gmail.com>
2024-05-15 15:43:38 -0500

    Update ut.hpp

--- include/ut/ut.hpp
+++ include/ut/ut.hpp
@@ -1,9 +1,7 @@
 // Refactored from:
 // Copyright (c) 2024 Kris Jusiak (kris at jusiak dot net)
-//
 // Distributed under the Boost Software License, Version 1.0.
-// (See accompanying file LICENSE_1_0.txt or copy at
-// http://www.boost.org/LICENSE_1_0.txt)
+// (See http://www.boost.org/LICENSE_1_0.txt)
 
 #pragma once
 
