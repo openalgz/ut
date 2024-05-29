8e9db249a9b2bb32d4a01b4298556a3317040e32
stephenberry <stephenberry@users.noreply.github.com>
2024-05-29 17:19:21 +0000

    Committing clang-format changes

--- src/main.cpp
+++ src/main.cpp
@@ -10,23 +10,23 @@ suite tests = [] {
       expect(42.1 == v) << "v is not 42.1";
       expect[42.1 == v] << "oh no!";
    };
-   
+
    "double mutable"_test = []() mutable {
       double v = 42.1;
       expect(42.1 == v) << "v is not 42.1";
       expect[42.1 == v] << "oh no!";
    };
-   
+
    "int"_test = [] {
       expect(5 + 4 == 9) << "bad";
       expect[5 + 4 == 9] << "fatal";
    };
-   
+
    "int consteval"_test = []() consteval {
       expect(5 + 4 == 9) << "bad";
       expect[5 + 4 == 9] << "fatal";
    };
-   
+
    "string"_test = [] {
       std::string_view v = "Hello World";
       expect(v == "Hello World");
